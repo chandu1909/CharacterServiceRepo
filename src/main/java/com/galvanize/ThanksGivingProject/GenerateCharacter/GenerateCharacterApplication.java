0cd0924dffe1f0f65ed88453a5c6acb4ca82f8dd
chandu1909 <chandra.polavarapu516@topper.wku.edu>
2018-11-20 16:34:03 -0700

    Initial Commit

---
+++ src/main/java/com/galvanize/ThanksGivingProject/GenerateCharacter/GenerateCharacterApplication.java
@@ -0,0 +1,12 @@
+package com.galvanize.ThanksGivingProject.GenerateCharacter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateCharacterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateCharacterApplication.class, args);
+	}
+}
